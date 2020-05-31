74ec8a8994d7284331aa2f7b7c90114ac507d515
samyak1021 <samdalal1021@gmail.com>
2020-05-31 20:20:41 +0530

    Add files via upload

---
+++ precache-manifest.e76f1e1c38a5a30c568a5b02b214f363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdadde452d5321b40f435b34d8699804",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49f721dfdc0ffe95eb28",
+    "url": "/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "dccd06ada54e0b286596",
+    "url": "/static/css/main.bd8858a0.chunk.css"
+  },
+  {
+    "revision": "49f721dfdc0ffe95eb28",
+    "url": "/static/js/2.595a61e0.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.595a61e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dccd06ada54e0b286596",
+    "url": "/static/js/main.77af7125.chunk.js"
+  },
+  {
+    "revision": "f5f5822b0683c7b3d7b7",
+    "url": "/static/js/runtime-main.7695a391.js"
+  }
+]);
